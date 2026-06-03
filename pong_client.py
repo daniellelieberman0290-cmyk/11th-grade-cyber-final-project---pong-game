b787b0a499ff204537658bf25fb59e0caffe3f78
USER <DanielleLieberman@Gmail.com>
2026-06-03 13:06:11 +0300

    initial version

--- pong_client.py
+++ pong_client.py
@@ -1,14 +1,16 @@
 """
 Author: Danielle Lieberman
 Program name: pong_client
-Description: gets players, ball and score coordinations and presents screen to player
+Description: gets players, ball and score Coordinates and presents screen to player
 Date: 03.05.26
 """
 
 import sys
-
 import pygame
-# from pygame.examples.aliens import Player
+import socket
+import os
+import pickle
+import logging
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -18,54 +20,112 @@ PLAYER_WIDTH  = 10
 PLAYER_HEIGHT = 100
 BALL_RADIUS = 15
 
-p1_y = 200
-ball_x = 10
-ball_y = 100
-ball_vx = .4
-ball_vy = -.4
+PORT = 2009
+LOG_DIR = "log"
+LOG_FILE_CLIENT = os.path.join(LOG_DIR, "client.log")
+os.makedirs(LOG_DIR, exist_ok=True)
+logging.basicConfig(filename=LOG_FILE_CLIENT,
+                    level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+logging.info("Client program started")
 
+
+# Create the client socket
+pong_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+logging.info('Trying to connect to server 127.0.0.1: ' + str(PORT))
+pong_client.connect(('127.0.0.1', PORT))
+logging.info('Connected to server')
+
+# Set a timeout so the loop does not get "stuck" waiting for data.
+pong_client.settimeout(0.1)
+
+# Get who am I (player 1 or player 2)
+player_id = int(pong_client.recv(2048).decode())
+logging.info("Received player id: " + str(player_id))
+
+print("Player ID:", player_id)
+print("Connected to server")
+
+
+
+# Building the graphics
 pygame.init()
-
-# setup
-win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+logging.info("Pygame initialized")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Pong Game')
 game_font = pygame.font.SysFont("monospace", 35)
 
-# TODO: create client (socket)
-
 run = True
+
+# Main loop
 while run:
-    # Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # the X button was clicked
+            logging.info("User clicked quit")
             run = False
-    # Input
+
+    # Send input to the server
+    msg = "NONE"
 
     # Read currently pressed keys
     # the variable "keys" will hold dictionary of bool for each key in the keyboard.
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_UP]:
-        p1_y -= .5
+        msg = "UP"
     if keys[pygame.K_DOWN]:
-        p1_y += .5
+        msg = "DOWN"
 
-    # Move ball
-    ball_x += ball_vx
-    ball_y += ball_vy
+    # Send the server my current direction
+    try:
+        pong_client.send(str.encode(msg))
+    except Exception as e:
+        logging.error("Failed sending message to server: " + str(e))
+        break
 
-    # Wall bounce (y axis: top / bottom)
-    if ball_y <=0 or ball_y >= (SCREEN_HEIGHT - BALL_RADIUS):
-        ball_vy *= -1
+    # Receive data from server.
+    try:
+        # First read the 4-byte header to know how big the incoming game state is.
+        header = pong_client.recv(4)
+        if not header:
+            continue
+
+        # Parse the header into int.
+        data_size =   int.from_bytes(header, byteorder='big')
+
+        # Then keep reading until we get the full payload
+        data = b""
+        while len(data) < data_size:
+            packet = pong_client.recv(data_size - len(data))
+
+            # Break in case of failure
+            if not packet:
+                break
+
+            # Build the data from each packet.
+            data += packet
+
+        # Convert the raw data into python variable.
+        game_state = pickle.loads(data)
+        if game_state["game_over"]:
+            logging.info("Game over received. Final score: player 1=" + str(game_state["p1_score"]) +
+                         ", player 2=" + str(game_state["p2_score"]))
+    except socket.timeout:
+        # No data received this time, just skip to draw
+        continue
+    except Exception as e:
+        logging.error("Failed receiving game state: " + str(e))
+        continue
 
     # Draw
-    win.fill((0, 0, 0)) #rgb color (red, green, blue)
+
+    screen.fill((0, 0, 0)) #rgb color (red, green, blue)
 
     # Vertical divider
     for y in range(0, SCREEN_HEIGHT, DIVIDER_HEIGHT * 2): # generate list of ys.
 
         # create rectangle
-        pygame.draw.rect(win, (255, 255, 255),  # color
+        pygame.draw.rect(screen, (255, 255, 255),  # color
                          (SCREEN_WIDTH / 2 - DIVIDER_WIDTH / 2,  # x position
                           y,  # y position
                           DIVIDER_WIDTH,  # the rectangle width
@@ -73,17 +133,20 @@ while run:
 
     # draw players
 
-    # left player.
-    pygame.draw.rect(win, (255,255,255), (0, # x coordinate
-                                          #(SCREEN_HEIGHT / 2) - (PLAYER_HEIGHT / 2), # 2 coordinate
-                                          p1_y,
+    # left player (player 1)
+    pygame.draw.rect(screen, (255,255,255), (0, # x coordinate
+                                          game_state["p1_y"],
                                           PLAYER_WIDTH, # width
                                           PLAYER_HEIGHT)) # height
-    pygame.draw.rect(win, (255, 255, 255), (SCREEN_WIDTH - PLAYER_WIDTH, (SCREEN_HEIGHT / 2) - (PLAYER_HEIGHT / 2), PLAYER_WIDTH, PLAYER_HEIGHT))
 
+    # right player (player 2)
+    pygame.draw.rect(screen, (255,255,255), (SCREEN_WIDTH - PLAYER_WIDTH, # x coordinate
+                                          game_state["p2_y"],
+                                          PLAYER_WIDTH, # width
+                                          PLAYER_HEIGHT)) # height
     # draw ball
-    pygame.draw.ellipse(win, (255, 255, 255), (ball_x, # x coordinate
-                                               ball_y, # y coordinate
+    pygame.draw.ellipse(screen, (255, 255, 255), (game_state["ball_x"], # x coordinate
+                                               game_state["ball_y"], # y coordinate
                                                BALL_RADIUS, # x radius
                                                BALL_RADIUS # y radius
                                                ))
@@ -91,14 +154,30 @@ while run:
     # draw score
 
     # generate the text graphics
-    score_text = game_font.render("0   0", # text to show
+    score_text = game_font.render(str(game_state["p1_score"]) + "   " +
+                                  str(game_state["p2_score"]) , # text to show
                                   True , # antialias
                                   (255, 255, 255)) # color
     # draw the graphics
-    win.blit(score_text, (SCREEN_WIDTH // 2 - 52, 20))
+    screen.blit(score_text, (SCREEN_WIDTH // 2 - 52, 20))
 
+    if game_state["game_over"]:
+        game_over_text = game_font.render( "Game Over",
+                                      True,  # antialias
+                                      (255, 255, 255))  # color
+        screen.blit(game_over_text, (SCREEN_WIDTH // 2 - 95, SCREEN_HEIGHT // 2 - 80))
+
+        winner = 1
+        if(game_state["p2_score"] > game_state["p1_score"]):
+            winner = 2
+        winner_text = game_font.render("player " + str(winner) + " wins",
+                                          True,  # antialias
+                                          (255, 255, 255))  # color
+        screen.blit(winner_text, (SCREEN_WIDTH // 2 - 130, SCREEN_HEIGHT // 2 - 55))
+    # Update the screen
     pygame.display.update()
 
 # Free resources
 pygame.quit()
+pong_client.close()
 sys.exit()
--- pong_server.py
+++ pong_server.py
@@ -1,59 +1,225 @@
+"""
+Author: Danielle Lieberman
+Program name: pong_server
+Description:creates players, ball and score Coordinates and sends them to clients
+Date: 08.05.26
+"""
+import pickle
 import socket
 import threading
 import time
-
-#TODO: logging
+import os
+import logging
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 DIVIDER_WIDTH = 4
 DIVIDER_HEIGHT = 20
-PLAYER_WIDTH  = 10
+PLAYER_WIDTH = 10
 PLAYER_HEIGHT = 100
 BALL_RADIUS = 15
+BALL_SPEED = 0.8
+PLAYER_SPEED = 1
+MAX_SCORE = 5
 
-server_ip = '127.0.0.1' # (localhost)
-port = 5555
+SERVER_IP = '127.0.0.1'  # (localhost)
+PORT = 2009
 
-game_state = {"p1_y":200,
-              "p2_y":200,
-              "ball_x":10,
-              "ball_y":100,
-              "ball_speed_x":.4,
-              "ball_speed_y":-.4,
-              "p1_score":0,
-              "p2_score":0
+LOG_DIR = 'log'
+LOG_FILE_SERVER = os.path.join(LOG_DIR, 'server.log')
+os.makedirs(LOG_DIR, exist_ok=True)
+logging.basicConfig(filename=LOG_FILE_SERVER,
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info('Server program started')
+
+game_state = {"p1_y": 200,
+              "p2_y": 200,
+              "ball_x": 10,
+              "ball_y": 100,
+              "ball_speed_x": BALL_SPEED,
+              "ball_speed_y": -BALL_SPEED,
+              "p1_score": 0,
+              "p2_score": 0,
+              "game_over": False
               }
+
+# Will contain the two clients connections
 connections = []
 
-def handle_client(conn, player_index):
-    global game_state
 
+def reset_ball_and_players():
+    logging.info("Reset ball and players")
+    # ball resetting
+    game_state["ball_x"] = SCREEN_WIDTH // 2
+    game_state["ball_y"] = SCREEN_HEIGHT // 2
+
+    # Change direction
+    game_state["ball_speed_x"] *= -1
+
+    # players resetting
+    game_state["p1_y"] = (SCREEN_HEIGHT / 2) - (PLAYER_HEIGHT / 2)
+    game_state["p2_y"] = (SCREEN_HEIGHT / 2) - (PLAYER_HEIGHT / 2)
+
+
+# player is 1 or 2
+def move_player_up(player_id):
+    logging.debug("Player " + str(player_id) + " moves up")
+    if player_id == 1:
+
+        if game_state["p1_y"] > 0:
+            game_state["p1_y"] -= PLAYER_SPEED
+
+    else:
+
+        if game_state["p2_y"] > 0:
+            game_state["p2_y"] -= PLAYER_SPEED
+
+
+def move_player_down(player_id):
+    logging.debug("Player " + str(player_id) + " moves down")
+    if player_id == 1:
+
+        if game_state["p1_y"] < SCREEN_HEIGHT - PLAYER_HEIGHT:
+            game_state["p1_y"] += PLAYER_SPEED
+
+    else:
+
+        if game_state["p2_y"] < SCREEN_HEIGHT - PLAYER_HEIGHT:
+            game_state["p2_y"] += PLAYER_SPEED
+
+
+# Runs when a client connects.
+def handle_client(conn, player_index):
+    print("\nhandle_client")
+    logging.info("Started handle_client for player " + str(player_index))
+    global game_state
+    # Tell the client if they are player 1 or 2
+    print("\nplayer_index=" + str(player_index))
+
+    conn.send(str.encode(str(player_index)))
+    logging.info("Sent player id " + str(player_index) + " to client")
+
+    # Receive the data from a client. (directions)
     while True:
         try:
             # Receive the data from the player
             data = conn.recv(2048).decode()
 
-            # TODO update player possition
-
             # Abort if no data exists
-            if not data: break
+            if not data:
+                break
 
+            # Update player position
+            # data can be "UP" or DOWN"
+            if data == "UP":
+                move_player_up(player_index)
+            if data == "DOWN":
+                move_player_down(player_index)
         except Exception as e:
+            logging.error("Error with player " + str(player_index) + ": " + str(e))
             break
+
+    logging.info("Player " + str(player_index) + " disconnected")
     conn.close()
 
+
 def game_logic():
+    logging.info("Game logic thread started")
     global game_state
 
-    # Move ball
-    game_state["ball_x"] += game_state["ball_speed_x"]
-    game_state["ball_y"] += game_state["ball_speed_y"]
+    while not game_state["game_over"]:
+        # Move ball
+        game_state["ball_x"] += game_state["ball_speed_x"]
+        game_state["ball_y"] += game_state["ball_speed_y"]
 
-    # Wall bounce (y-axis: top / bottom)
-    if game_state["ball_y"] <= 0 or game_state["ball_y"] >= (SCREEN_HEIGHT - BALL_RADIUS):
-        game_state["ball_speed_y"] *= -1
+        # Wall ball bounce (y-axis: top / bottom)
+        if game_state["ball_y"] <= 0 or game_state["ball_y"] >= (SCREEN_HEIGHT - BALL_RADIUS):
+            game_state["ball_speed_y"] *= -1
+            logging.info("Ball bounced on top/bottom wall")
 
-    # player 1 collision
-    if game_state["ball_x"] < PLAYER_WIDTH and game_state["ball_y"] < game_state["ball_y"] and game_state["ball_y"] > (game_state["ball_y"] - PLAYER_HEIGHT):
-        game_state["ball_speed_x"] *= -1
+        # player 1 collision
+        if (game_state["ball_x"] <= PLAYER_WIDTH and
+                game_state["p1_y"] < game_state["ball_y"] < (game_state["p1_y"] + PLAYER_HEIGHT)):
+            game_state["ball_speed_x"] *= -1
+            logging.info("Ball hit player 1 paddle")
+
+            # Prevent sticking
+            game_state["ball_x"] = PLAYER_WIDTH
+
+        # player 2 collision
+        if (game_state["ball_x"] >= (SCREEN_WIDTH - PLAYER_WIDTH - BALL_RADIUS) and
+                game_state["p2_y"] < game_state["ball_y"] < (game_state["p2_y"] + PLAYER_HEIGHT)):
+            game_state["ball_speed_x"] *= -1
+            logging.info("Ball hit player 2 paddle")
+
+            # Prevent sticking
+            game_state["ball_x"] = SCREEN_WIDTH - PLAYER_WIDTH - BALL_RADIUS
+
+        # Goal left
+        if game_state["ball_x"] < 0:
+            game_state["p2_score"] += 1
+            logging.info("Goal left: player 2 score is " + str(game_state["p2_score"]))
+
+            reset_ball_and_players()
+
+        # Goal right
+        if game_state["ball_x"] > SCREEN_WIDTH:
+            game_state["p1_score"] += 1
+            logging.info("Goal right: player 1 score is " + str(game_state["p1_score"]))
+
+            reset_ball_and_players()
+
+        time.sleep(0.015)  # wait ~ 1 / 60 of a second.
+
+
+pong_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pong_server.bind((SERVER_IP, PORT))
+# start listen to incoming connection no more than 2 connections at one time
+pong_server.listen(2)
+logging.info("Server listening on " + str(SERVER_IP) + ": " + str(PORT))
+
+for i in range(2):
+    print("\naccepting " + str(i))
+    # A client has been call the server first the first time.
+    conn, addr = pong_server.accept()
+
+    # Save the connection object for future communications.
+    connections.append(conn)
+    logging.info("Client connected from " + str(addr) + " as player " + str(i + 1))
+
+    # Create a new thread which runs in parallel the "handle_clien" function with those arguments:
+    # conn: the client connection
+    # i   : player index (using the for loop index)
+    thread = threading.Thread(target=handle_client, daemon=True, args=(conn, i + 1))
+
+    # Start running the thread.
+    thread.start()
+
+    print("\nplayer " + str(i + 1) + " connected")
+    logging.info("Player " + str(i + 1) + " connected")
+
+# Start running the game.
+threading.Thread(target=game_logic, daemon=True).start()
+
+# while the game is running send each player the game state
+#while ((game_state["p1_score"] < 5) and (game_state["p2_score"] < 5)):
+while not game_state["game_over"]:
+    game_state["game_over"] = not ((game_state["p1_score"] < MAX_SCORE) and (game_state["p2_score"] < MAX_SCORE))
+    data = pickle.dumps(game_state)
+
+    # Path the size of the data into 4 bytes.
+    data_length = len(data).to_bytes(4, byteorder="big")
+
+    for conn in connections:
+        try:
+            # Send size + data
+            conn.sendall(data_length + data)
+        except Exception as e:
+            logging.error("Failed sending game state to client: " + str(e))
+            pass
+    time.sleep(0.015)
+
+logging.info("Game over. Final score: player 1=" + str(game_state["p1_score"]) + ", player 2=" +
+             str(game_state["p2_score"]))
+input("\npress enter to quit")
